1d21aae048f2d64ac4bc1bfb162493dada65a03b
ericchase <github@eric.addymail.com>
2024-04-03 19:52:07 -0500

    format
    Signed-off-by: ericchase <github@eric.addymail.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,13 +4,13 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+  format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .plugin(tauri_plugin_shell::init())
+    .invoke_handler(tauri::generate_handler![greet])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
